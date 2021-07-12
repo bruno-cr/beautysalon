f1fd22a177b7515449009a2e0d46f25f9dd03397
Bruno Camargo <bruno.cr@outlook.com>
2021-07-12 13:04:03 -0300

    Rodapé e botão voltar para cima

--- main.js
+++ main.js
@@ -18,16 +18,18 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll(){
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
+
 
 /* Testimonials swiper */
 const swiper = new Swiper('.swiper-container', {
@@ -52,7 +54,25 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
-)
+  )
+  
+  /* Botão voltar para o topo */
+
+  function backToTop(){
+    const backToTopButton = document.querySelector('.back-to-top')
+    if (window.scrollY >= 560) {
+      backToTopButton.classList.add('show')
+    } else{
+      backToTopButton.classList.remove('show')
+    }
+  }
+
+  /* When Scroll */
+  window.addEventListener('scroll', function () {
+    changeHeaderWhenScroll()
+    backToTop()
+  })
